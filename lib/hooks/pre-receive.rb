9316f16eb49f7f4b4a5d8a5e9019a515a5a19c4d
blaines <github@technologyaz.com>
2011-02-21 10:10:49 -0800

    refactoring

--- lib/hooks/pre-receive.rb
+++ lib/hooks/pre-receive.rb
@@ -1,6 +1,7 @@
 #!/bin/sh
+read stdin
 declare -x rvm_path="/usr/local/rvm"
 if [[ -s /usr/local/rvm/scripts/rvm ]] ; then source /usr/local/rvm/scripts/rvm ; fi
 rvm 1.8.7@base
 
-git-deploy deploy:hook
+echo -e $stdin | git-deploy deploy:hook
