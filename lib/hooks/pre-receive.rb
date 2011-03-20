f92462997baac754d884ea94b6e4660a2dff3ecd
blaines <github@technologyaz.com>
2011-03-20 02:26:56 -0700

    Changed to ree@base

--- lib/hooks/pre-receive.rb
+++ lib/hooks/pre-receive.rb
@@ -2,6 +2,6 @@
 read stdin
 declare -x rvm_path="/usr/local/rvm"
 if [[ -s /usr/local/rvm/scripts/rvm ]] ; then source /usr/local/rvm/scripts/rvm ; fi
-rvm 1.8.7@base
+rvm ree@base
 
 echo -e $stdin | git-deploy deploy:hook
